5a400277976b83bc27048d90e8acc6a6e5319f09
mahan <m.mahmodi1380@gmail.com>
2022-10-08 06:33:53 +0330

    using query params.

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,7 +1,13 @@
+import { useRouter } from 'next/router';
 const PortfolioProjectPage = () => {
+  const router = useRouter();
+
   return (
     <div>
-      <h1>the dynamic portfolio project page</h1>
+      <h1>
+        the dynamic portfolio project page with the project id of :{' '}
+        {router.query.projectid}
+      </h1>
     </div>
   );
 };
