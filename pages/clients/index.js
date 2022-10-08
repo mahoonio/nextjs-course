2f66dc8e6b072ae542b62af99d0c3dde7aebf3f8
mahan <m.mahmodi1380@gmail.com>
2022-10-08 19:47:53 +0330

    dynamic navigation.

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+
 const ClientsPage = () => {
+  const clients = [
+    { id: 'mah', name: 'mahan' },
+    { id: 'roz', name: 'roozbeh' },
+  ];
   return (
     <div>
       <h1>clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
