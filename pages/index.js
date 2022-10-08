42597d4cf59507fbbc7dee91ba4341962ccf56db
mahan <m.mahmodi1380@gmail.com>
2022-10-08 19:41:34 +0330

    navigation with Link component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          {' '}
+          <Link href={'/portfolio'}>portfolio</Link>
+        </li>
+        <li>
+          <Link href={'/clients'}>clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
