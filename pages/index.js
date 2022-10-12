ae35f6d95df58073179b41dcf40315e46657f7b4
mahan <m.mahmodi1380@gmail.com>
2022-10-12 21:07:54 +0330

    adding getStaticProps.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
 import Link from 'next/link';
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const { products } = props;
   return (
     <div>
-      <h1>The Home Page</h1>
       <ul>
-        <li>
-          {' '}
-          <Link href={'/portfolio'}>portfolio</Link>
-        </li>
-        <li>
-          <Link href={'/clients'}>clients</Link>
-        </li>
+        {products.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
       </ul>
     </div>
   );
 };
 
+export async function getStaticProps() {
+  const products = [{ id: 1, title: 'product 1' }];
+  return { props: { products } };
+}
 export default HomePage;
--- pages/portfolio/list.js
+++
@@ -1,9 +0,0 @@
-const ListPage = () => {
-  return (
-    <div>
-      <h1>The list page is in a nested path</h1>
-    </div>
-  );
-};
-
-export default ListPage;
