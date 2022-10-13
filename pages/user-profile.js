8342541b1bae90a4ebf6d1615b543174d97e14c4
mahan <m.mahmodi1380@gmail.com>
2022-10-14 03:05:18 +0330

    getServerSide Props.

---
+++ pages/user-profile.js
@@ -0,0 +1,11 @@
+const UserProfilePage = (props) => {
+  return <h1>{props.userName}</h1>;
+};
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  return {
+    props: { userName: 'mahan' },
+  };
+}
