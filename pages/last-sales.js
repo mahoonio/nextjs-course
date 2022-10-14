ac9cc4677585523db50101b2a3ccdbb64c839bed
mahan <m.mahmodi1380@gmail.com>
2022-10-14 17:09:10 +0330

    useSwr hook.

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Parse from 'parse';
+import useSWR from 'swr';
 
 // Your Parse initialization configuration goes here
 const PARSE_APPLICATION_ID = 'fYBYIgt7jUjTpeVrer06mpll4G2bb8Ed7XTLsMm5';
@@ -8,33 +9,52 @@ const PARSE_JAVASCRIPT_KEY = 'zcp4kj9FtBCxZje3oYhd5QtINYZMaVILHQ7SXoEo';
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 const LastSales = () => {
-  const [isLoading, setIsLoading] = useState(false);
+  //   const [isLoading, setIsLoading] = useState(false);
   const [sales, setSales] = useState();
+
+  const { data, error } = useSWR(
+    'https://jsonplaceholder.typicode.com/users',
+    (url) => {
+      return fetch(url, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((response) => response.json());
+    }
+  );
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetchSales().then((res) => {
+  //       setSales(res);
+  //       setIsLoading(false);
+  //     });
+  //   }, []);
+
   useEffect(() => {
-    setIsLoading(true);
-    fetchSales().then((res) => {
-      setSales(res);
-      setIsLoading(false);
-    });
-  }, []);
-  if (isLoading) {
+    if (data) {
+      const transformedData = data.map((user) => ({
+        name: user.name,
+        volume: Math.round(Math.abs(user.address.geo.lat)),
+      }));
+
+      setSales(transformedData);
+    }
+  }, [data]);
+
+  if (error) {
+    return <p>failed to fetch</p>;
+  }
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>no data yet...</p>;
-  }
-  if (sales) {
-    console.log('asdea', sales);
-  }
   return (
     <ul>
-      {sales &&
-        sales.map((sale) => (
-          <li>
-            {sale.name} - ${sale.volume}
-          </li>
-        ))}
+      {sales.map((sale) => (
+        <li key={sale.id}>
+          {sale.name} - ${sale.volume}
+        </li>
+      ))}
     </ul>
   );
 };
@@ -50,12 +70,18 @@ async function fetchSales() {
   query.equalTo('objectId', salesObjIds[1].id);
   const secondObj = await query.first();
   const array = [
-    { name: firstObj.get('name'), volume: firstObj.get('volume') },
-    { name: secondObj.get('name'), volume: secondObj.get('volume') },
+    {
+      name: firstObj.get('name'),
+      volume: firstObj.get('volume'),
+      id: firstObj.id,
+    },
+    {
+      name: secondObj.get('name'),
+      volume: secondObj.get('volume'),
+      id: secondObj.id,
+    },
   ];
 
   return array;
   //   const obj = { name: sale.get('name'), volume: sale.get('volume') };
-  console.log(salesObjIds);
-  console.log('arr : ', array);
 }
