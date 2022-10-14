2d1a0a2c2bef53ffb739140f438b3e62d5ea03aa
mahan <m.mahmodi1380@gmail.com>
2022-10-14 06:01:22 +0330

    initializing back4app dummy backend.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Parse from 'parse/dist/parse.min.js';
+
+// Your Parse initialization configuration goes here
+const PARSE_APPLICATION_ID = 'fYBYIgt7jUjTpeVrer06mpll4G2bb8Ed7XTLsMm5';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'zcp4kj9FtBCxZje3oYhd5QtINYZMaVILHQ7SXoEo';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
